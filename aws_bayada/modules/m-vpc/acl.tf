471ffed55509d031ee2e54bf3c900688544bfeb8
BAYADAWebServices <46054009+jdubFromBayada@users.noreply.github.com>
2019-08-01 16:44:15 -0400

    added acl file

--- aws_bayada/modules/m-vpc/acl.tf
+++ aws_bayada/modules/m-vpc/acl.tf
@@ -1,3 +1,47 @@
+resource "aws_default_network_acl" "acl_default" {
+
+  default_network_acl_id = "${aws_vpc.main.default_network_acl_id}"
+
+  tags = {
+    Name              = "acl${var.tag_name}"
+	"bws:Description" = "Default Network ACL to allow all traffic in and out"
+    "bws:Service"     = "${var.tag_service}"
+    "bws:Office"      = "${var.tag_office}"
+    "bws:Environment" = "${var.tag_environment}"
+    "bws:Customer"    = "${var.tag_customer}"
+	
+  }
+  
+}
+
+resource "aws_network_acl_rule" "acl_rule_inbound_default" {
+
+  network_acl_id = "${aws_default_network_acl.acl_default.id}"
+  rule_number    = 100
+  egress         = false
+  protocol       = "-1"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 0
+  to_port        = 0
+  
+}
+
+resource "aws_network_acl_rule" "acl_rule_outbound_default" {
+
+  network_acl_id = "${aws_default_network_acl.acl_default.id}"
+  rule_number    = 100
+  egress         = true
+  protocol       = "-1"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 0
+  to_port        = 0
+  
+}
+
+
+/*
 resource "aws_network_acl" "main" {
 
   vpc_id = "${aws_vpc.main.id}"
@@ -38,3 +82,4 @@ resource "aws_network_acl_rule" "acl_rule_outbound_main" {
   to_port        = 0
   
 }
+*/
