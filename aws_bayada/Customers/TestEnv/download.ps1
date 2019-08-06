10cb86bf7c3e09f229fc8b9eb4c76c497fdb9164
jwilcox@bayada.com <46054009+jdubFromBayada@users.noreply.github.com>
2019-08-06 10:45:35 -0400

    Removed download.ps1 file

--- aws_bayada/Customers/TestEnv/download.ps1
+++
@@ -1,8 +0,0 @@
-$c_downloads = "c:\downloads\"
-
-if((test-path $c_downloads) -eq $false) {
-    New-Item -Path "C:\" -Name "downloads" -ItemType "directory"
-    write-host "Folder created: $c_downloads"
-} else {
-    write-host "Folder exists: $c_downloads"
-}
