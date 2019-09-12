e49c70015cdfc774a10e9c6a41477b72ceba3709
Steve Styerwalt <steve@foghornconsulting.com>
2019-09-11 17:08:27 -0700

    added ami sharing code2

--- aws_bayada/Customers/TestEnv/ami_share.tf
+++ aws_bayada/Customers/TestEnv/ami_share.tf
@@ -1,10 +1,10 @@
-resource "aws_ami_launch_permission" "share_web_ami" {
+resource "aws_ami_launch_permission" "share_web1_ami" {
   provider   = "aws.ses_shared"
   image_id   = "${var.ami_web1}"
   account_id = "${var.account_id}"
 }
 
-resource "aws_ami_launch_permission" "share_web_ami" {
+resource "aws_ami_launch_permission" "share_web2_ami" {
   provider   = "aws.ses_shared"
   image_id   = "${var.ami_web2}"
   account_id = "${var.account_id}"
